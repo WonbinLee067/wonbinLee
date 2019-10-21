09f1df4d22cc5581597eae38bf5cead8bca2ff46
WonbinLee067 <smilelwb067@gmail.com>
2019-10-21 13:38:40 +0900

    create Let me Introduce program!

---
+++ introduce.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello, My name is Wonbin Lee\n");
+	printf("My major is Software and I'm interested in AI, now.\n");
+	printf("Please feedback me a lot about what I'm doing in github.\n");
+}
