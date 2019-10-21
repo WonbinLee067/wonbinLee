c5a1809281a30a3ec2fda0c28b5eed0af8a974c4
WonbinLee067 <smilelwb067@gmail.com>
2019-10-21 13:47:03 +0900

    create AI interest program!

---
+++ ai.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("There are a lot of fields in AI\n");
+	printf("For example, machine learning, deep learning, vision etc\n");
+	printf("I want study deeply about deep learning.\n");
+}
