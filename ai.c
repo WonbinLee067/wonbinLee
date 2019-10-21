515e1d4b7bc59d098cf2cd4a252914275b2a16fa
WonbinLee067 <smilelwb067@gmail.com>
2019-10-21 14:01:30 +0900

    modified commit

--- ai.c
+++ ai.c
@@ -4,4 +4,5 @@ int main(void) {
 	printf("There are a lot of fields in AI\n");
 	printf("For example, machine learning, deep learning, vision etc\n");
 	printf("I want study deeply about deep learning.\n");
+	printf("Please give me a lot of sources about it!\n");
 }
